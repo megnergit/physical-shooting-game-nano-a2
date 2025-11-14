105ad0494f1d7cc602cce71f2f8741d9ab33c4ed
Meg <miwa.egner@gmail.com>
2025-11-14 12:59:57 +0100

    Include images.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Physical Shooting Game with ESP32
+# Physical Shooting Game with Arduino Nano
 
 ## Table of Contents 
 
@@ -21,10 +21,13 @@ Okay, let us create one.
 
 For a target,
 
-- ESP32 board
+- Arduino Nano
+- USB mini-B (to write code on Arduino Nano)
 - photoresistor (LDR: Light Dependent Resistor) 
 - LED
 - active speaker
+- Resistor (220 Ω. For LED)
+p- Resistor (10 kΩ. For photoresistor)
 - wires and pins
 - Aluminum tape (to make a physical target)
 
@@ -40,6 +43,134 @@ and my Charlie (= ChatGPT) will write a code.
 
 ---
 ## Wiring - Target
+
+![wiring](./images/wiring-1.jpg)
+
+Connect the signal lines as follows. 
+
+|                 |    |
+|-----------------|----|
+| photoresistor   | A0 |
+| active speaker  | D9 | 
+| LED             | D8 |
+
+For the photoresistor, make sure to insert 10 kΩ between the 
+signal line and the ground. The left most column of the breadboard 
+in the picture was used to connect LDR 'S' and 10 kΩ.
+
+```sh
+    5V
+    | (white line in the picture above)
+    |
+    |     (black. labeled with '-')
+ [ LDR ] ---> GND
+    |
+    | (gray. 'S')
+    |-------> A0 (Analog Input)
+    |
+ [10 kΩ]
+    |
+   GND
+```
+
+For the LED, put 220 Ω between '+' and 5V. 
+```sh
+    5V
+    | 
+    | 
+ [220 Ω]
+    |
+    | (longer leg)
+ [ LED ] 
+    | (shorter leg)
+    |
+   GND
+```
+
+![speaker and LDR](./images/wiring-2.jpg)
+![close-up](./images/wiring-3.jpg)
+
+---
+## Code
+
+The directory structure is 
+
+```sh
+$ tree . -L 2
+.
+├── images
+│   ├── board-1.png
+│   ├── wiring-1.jpg
+│   ├── wiring-2.jpg
+│   └── wiring-3.jpg
+├── LICENSE
+├── nano_shooting
+│   └── nano_shooting.ino
+└── README.md
+
+3 directories, 7 files
+$
+```
+
+Upload the code ```nano_shooting.ino```. 
+
+**Note** The board, port and processor settings are as follows. 
+The port will be different from yours. 
+
+|                 |              |             | 
+|-----------------|--------------| ------------|
+| Board           | Arduino Nano | 
+| Port            | /dev/cu.usbseriral-1410 | for my MacBook |
+| Processor       | ATmega328P (Old Bootloader) | pick 'Old...' one|
+
+![board settings](./images/board-1.png)
+
+In case  you got this kind of error message, press the reset button 
+on Nano (small white rectangular button in the middle).
+
+```sh
+Sketch uses 3984 bytes (12%) of program storage space. Maximum is 30720 bytes.
+Global variables use 320 bytes (15%) of dynamic memory, leaving 1728 bytes for local variables. Maximum is 2048 bytes.
+avrdude: stk500_getsync() attempt 1 of 10: not in sync: resp=0x4c
+avrdude: stk500_getsync() attempt 2 of 10: not in sync: resp=0x44
+...
+Failed uploading: uploading error: exit status 1
+```
+
+---
+## Adjust threshold
+
+The photoresistor outputs an analog signal between 0 and 1023, 
+the brighter the light it receives, the smaller the signal. 
+
+To avoid the speaker and the LED being activated with ambient light, 
+we have to set the threshold correctly. 
+
+Uncomment the line ```Serial.println(v);``` and upload the code 
+again. Open serial monitor and measure the ambient brightness, 
+and the hit brightness (use your laser point to illuminate the photoresistor). 
+
+
+```cpp
+...
+void loop(){
+  // read the LDR and average the signal
+  int v = readA0Avg();
+  // uncomment to measure the anbient light. 
+  Serial.println(v);  
+...  
+```
+
+Then set the following parameters. 
+
+```cpp
+...
+// ---- thresholds. need to adjust 
+int onThr  = 96;  // if smaller than this, counted as a hit.
+int offThr = 128;  // if larger than this, target is reset (= ready to get shot).
+...
+```
+
 
 
 
---
+++ nano_shooting/nano_shooting.ino
@@ -0,0 +1,166 @@
+// ====== LDR + Melody (Arduino Nano, Passive Buzzer on D8) ======
+#define C4  262
+#define D4  294
+#define E4  330
+#define G4  392
+#define A4  440
+#define H4  494
+
+#define C5  523
+#define D5  587
+#define E5  659
+#define F5  698
+#define FS5 740
+#define G5  784
+#define A5  880
+#define H5  988
+#define C6  1047
+#define D6  1175
+#define E6  1319
+#define NN  0     // no sound
+
+const int BUZZER_PIN = 9;   // active busser
+const int LED_PIN = 8;
+const int SENSOR_PIN = A0;  // read LDR partial voltage
+
+//int dr1 = 150;              // length of one tone
+int dr1 = 150;              // length of one tone
+unsigned long coolDownMs = 500; // avoid multiple hits at a time
+
+// ---- thresholds. need to adjust 
+int onThr  = 96;  // if smaller than this, counted as a hit.
+int offThr = 128;  // if larger than this, target is reset (= ready to get shot). 
+
+bool beamOn = true; // at start, target is assumed to be in 'hit'-state.
+bool waitingForReset = false;
+bool last = true; 
+
+// average signals to reduce noise.
+int readA0Avg(uint8_t n=8){
+  long s = 0;
+  long a = 0;
+  for(uint8_t i=0;i<n;i++){ 
+    a = analogRead(A0);
+//    s += analogRead(A0);
+    s += a;
+//    Serial.println(a);
+    delay(1); 
+    }
+  return (int)(s / n);
+}
+
+// Popcorn (https://www.youtube.com/watch?v=NjxNnqTcHhg)
+int mel_pop[] = {
+  A5, G5, 
+  A5, E5, C5, E5, A4, NN, A5, G5, 
+  A5, E5, C5, E5, A4, NN, A5, H5, 
+  C6, H5, C6, A5, H5, A5, H5, G5, 
+  A5, G5, A5, F5, A5, NN, A5, G5, 
+  A5, E5, C5, E5, A4, NN, A5, G5, 
+  A5, E5, C5, E5, A4, NN, A5, H5, 
+  C6, H5, C6, A5, H5, A5, H5, G5, 
+  A5, G5, A5, H5, C6, NN, E5, D5,
+  E5, C5, G4, C5, E4, NN, E5, D5, 
+  E5, C5, G4, C5, E4, NN, E5, FS5, 
+  G5, FS5, G5, E5, FS5, E5, FS5, D5,
+  E5, D5, E5, C5, E5, NN, E5, D5, 
+  E5, C5, G4, C5, E4, NN, E5, D5, 
+  E5, C5, G4, C5, E4, NN, E5, FS5, 
+  G5, FS5, G4, E5, FS5, E5, FS5, D5,
+  E5, D5, C5, D5, E5, NN, NN, NN,
+  A4, C5, A4, C5, A4, C5, A4, C5, 
+  A4, NN, E5, NN, A5, NN, NN, NN,
+  NN, NN, NN, NN, NN, NN, NN, NN
+};
+
+// interstellar
+int mel_ins[] = {
+  E6, H5, C6, D6,
+  E6, H5, C6, D6,
+  E6, H5, C6, D6,
+  E6, NN, NN, NN,
+  E6, NN, NN, NN,
+  E6, NN, NN, NN,
+  E6, NN, NN, NN,
+  E6, NN, NN, NN,
+  E6, NN, NN, NN
+};
+  
+// Popcorn but only for first two tacts.
+int mel[] = {
+  A5, G5, 
+  A5, E5, C5, E5, A4, NN 
+};
+void playOne(int freq, int dur_ms){
+  if(freq > 0){
+    tone(BUZZER_PIN, freq);
+  }else{
+    noTone(BUZZER_PIN);
+  }
+  delay(dur_ms);
+  noTone(BUZZER_PIN);
+}
+
+void playMelody(){
+  const int n = sizeof(mel)/sizeof(mel[0]);
+  for(int i = 0; i < n; i++){
+    playOne(mel[i], dr1);
+  }
+}
+
+void setup(){
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
+  Serial.begin(115200);
+
+  Serial.println("LDR + Melody test start");
+
+  // initizlize the target
+  int v0 = readA0Avg();
+  beamOn = (v0 < onThr);  // smaller the value, the brighter the surroundings.
+  waitingForReset = false; 
+  last = beamOn;          // wait for the first hit
+
+}
+
+
+void loop(){
+  // read the LDR and average the signal
+  int v = readA0Avg();
+  // uncomment to measure the anbient light. 
+  Serial.println(v);  
+
+  // current beam state.
+  if(!beamOn && v < onThr)  beamOn = true;   // hit
+  if(beamOn  && v > offThr) beamOn = false;  // deactivate LDR
+
+ 
+  // while the target is hit, but LDR is innactive, do nothing.
+  if(waitingForReset){
+    if(beamOn){
+      // when LDR is reset, you may hit again.
+      waitingForReset = false;
+      Serial.println("RESET");  // show the status to serial monitor
+    }
+    delay(2);
+    return;
+  }
+
+  // wait here until the game is started, or LDR is active again.
+  static bool last = true;
+  if(last && beamOn){
+    // 'hit' makes LDR deactive in the same time
+    Serial.print("HIT! A0="); Serial.println(v);
+    
+    // LED
+    digitalWrite(LED_PIN, HIGH);
+    playMelody();
+    digitalWrite(LED_PIN, LOW);
+
+    waitingForReset = true;  // wait. 
+  }
+  last = beamOn;
+
+  delay(2);
+}
